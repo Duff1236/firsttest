c7e56a493a08fbe96bc3c37b109f37666c2bc20d
Duff1236 <leadev77@mail.ru>
2019-11-06 22:30:45 +0300

    first commit

---
+++ firsttest.py
@@ -0,0 +1,18 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.common.keys import keys
+
+driver = webdriver.Chrome()
+driver.get("http://mail.ru")
+element = driver.find_element_by_name("login")
+element.send_keys("9629460714")
+element.send_keys(Keys.RETURN)
+felement = driver.find_element_by_name("password")
+#felement.send_keys("")
+felement.send_keys(Keys.RETURN)
+selement = driver.find_element_by_xpath("//input[@class ='b-toolbar__btn js-shortcut']")
+driver.get("http://e.mail.ru" + selement.get_attribute("href"))
+selement.click()
+driver.close()
+
